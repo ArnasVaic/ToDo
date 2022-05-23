4279512a3415e6ee4ac28b9450edb810a7086d34
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-23 17:37:21 +0300

    Create ToDoRepositoryFile.cs

---
+++ ToDo/Dependencies/ToDoRepositoryFile.cs
@@ -0,0 +1,33 @@
+﻿using ToDo.Models;
+
+namespace ToDo.Dependencies
+{
+    public class ToDoRepositoryFile : IToDoRepository
+    {
+
+        public void AddPost(ToDoPostModel post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsPost(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePost(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ToDoPostModel GetPost(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReplacePost(ToDoPostModel post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
