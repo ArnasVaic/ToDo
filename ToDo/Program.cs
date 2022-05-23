0098dc6be6626f505c43e1ff12388a225637c3db
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-23 17:35:09 +0300

    Update Program.cs

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,19 +1,16 @@
-using ToDo2.Dependencies;
+using ToDo.Dependencies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<PostDependency>();
+builder.Services.AddSingleton<IToDoRepository, ToDoRepository>();
 
 var app = builder.Build();
-
-//app.AddSingleton<, >();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
