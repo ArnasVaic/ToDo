5c1c7e9889cc6a7b0cee975dfd91aed958b756e4
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-23 14:05:38 +0300

    Create ToDoPost.cs

---
+++ ToDo/ToDoPost.cs
@@ -0,0 +1,9 @@
+namespace ToDo2
+{
+    public class ToDoPost
+    {
+        public String Title { get; set; }
+        public DateTime Deadline { get; set; }
+        public String Description { get; set; }
+    }
+}
