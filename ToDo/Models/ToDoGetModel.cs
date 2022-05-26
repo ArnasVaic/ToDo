47f5ac243f33f255095ecbebd47b7a10b1a0c816
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-26 16:22:17 +0300

    Create ToDoGetModel.cs

---
+++ ToDo/Models/ToDoGetModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace ToDo.Models
+{
+    [DebuggerDisplay("Title:{Title}\nDeadline:{Deadline}\nDescription:{Description}\nId:{Id}")]
+    public class ToDoGetModel
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string Title { get; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string Description { get; }
+
+        [Required()]
+        public DateTime Deadline { get; }
+
+        [Required()]
+        public int Id { get; }
+    }
+}
