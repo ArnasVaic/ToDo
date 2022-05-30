746b6b60fdfa5a4baf9a5e9df499f92215b0c37c
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2022-05-30 15:04:37 +0300

    Create IGenericRepository.cs

---
+++ ToDo/Dependencies/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace ToDo.Dependencies
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T? GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        bool Add(T entity);
+        bool AddRange(IEnumerable<T> entities);
+        bool Remove(T entity);
+        bool RemoveRange(IEnumerable<T> entities);
+    }
+}
